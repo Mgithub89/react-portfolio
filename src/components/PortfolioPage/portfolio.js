faaf35b2be2377a034213b6f50077d5e52fe0920
metages worku <metages09@gmail.com>
2021-02-05 11:06:14 -0800

    edit portfolio page

--- src/components/PortfolioPage/portfolio.js
+++ src/components/PortfolioPage/portfolio.js
@@ -4,7 +4,7 @@ import "./style.css";
 export default function portfolio() {
     return (
         <div>
-            <main class="container m-5 p-5" id="portfolio">
+            <main class="container m-5 p-5" id="portfolio" >
                 <div class="row">
                     <div class="col">
                         <h1 class="text-info font-weight-bold">Portfolio</h1>
@@ -19,7 +19,7 @@ export default function portfolio() {
                         <figure class="figure ">
                             <h4>Weather-Dashboard</h4>
                             <a href="https://mgithub89.github.io/Weather-Dashboard/" class="streched-link" target="-blank">
-                                <img src="Assets/images/weather-app.PNG" class="figure-img img-fluid" alt="image3" />
+                                <img src="Assets/images/weather-app.PNG" class="figure-img img-fluid pro" alt="image3" />
                             </a>
                         </figure>
                     </div>
@@ -27,7 +27,7 @@ export default function portfolio() {
                         <figure class="figure">
                             <h4>Day planner</h4>
                             <a href="https://mgithub89.github.io/Work-Day-Scheduler/" class="streched-link" target="-blank">
-                                <img src="Assets/images/Day planner.PNG" class="figure-img img-fluid" alt="image2" />
+                                <img src="Assets/images/Day planner.PNG" class="figure-img img-fluid pro" alt="image2" />
                             </a>
                         </figure>
                     </div>
@@ -38,7 +38,7 @@ export default function portfolio() {
                         <figure class="figure">
                             <h4>Password-Generator</h4>
                             <a href="https://mgithub89.github.io/Password-Generator/" class="streched-link" target="-blank">
-                                <img src="Assets/images/password generator.PNG" class="figure-img img-fluid" alt="image3" />
+                                <img src="Assets/images/password generator.PNG" class="figure-img img-fluid pro" alt="image3" />
                             </a>
                         </figure>
                     </div>
@@ -46,7 +46,7 @@ export default function portfolio() {
                         <figure class="figure">
                             <h4>Project1-!Bored</h4>
                             <a href="https://mgithub89.github.io/UWB-ActivityApp/" class="streched-link" target="-blank">
-                                <img src="Assets/images/Project1-!Bored.PNG" class="figure-img img-fluid" alt="image4" />
+                                <img src="Assets/images/Project1-!Bored.PNG" class="figure-img img-fluid pro" alt="image4" />
                             </a>
                         </figure>
                     </div>
@@ -57,7 +57,7 @@ export default function portfolio() {
                         <figure class="figure">
                             <h4>Code-Quiz</h4>
                             <a href="https://mgithub89.github.io/CodeQuiz-Game/" class="streched-link" target="-blank">
-                                <img src="Assets/images/quiz.PNG" class="figure-img img-fluid" alt="image5" />
+                                <img src="Assets/images/quiz.PNG" class="figure-img img-fluid pro" alt="image5" />
                             </a>
                         </figure>
                     </div>
@@ -65,7 +65,7 @@ export default function portfolio() {
                         <figure class="figure">
                             <h4>Eat-Da-Burger</h4>
                             <a href="https://evening-dawn-38535.herokuapp.com/" class="streched-link" target="-blank">
-                                <img src="Assets/images/Eat.PNG" class="figure-img img-fluid" alt="image5" />
+                                <img src="Assets/images/Eat.PNG" class="figure-img img-fluid pro" alt="image5" />
                             </a>
                         </figure>
                     </div>
@@ -75,7 +75,7 @@ export default function portfolio() {
                         <figure class="figure">
                             <h4>Project2-DigitalElf</h4>
                             <a href="https://fast-anchorage-28495.herokuapp.com/" class="streched-link" target="-blank">
-                                <img src="Assets/images/Project2.PNG" class="figure-img img-fluid" alt="image5" />
+                                <img src="Assets/images/Project2.PNG" class="figure-img img-fluid pro" alt="image5" />
                             </a>
                         </figure>
                     </div>
